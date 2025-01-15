90d8d0ae42776b98161c3c72b65c4f0259efcec4
HEEHAMTARO <jaonai0954400179@gmail.com>
2025-01-15 09:19:11 +0700

    add - close #2

--- backend/entity/users.go
+++ backend/entity/users.go
@@ -7,4 +7,5 @@ import (
 type User struct{
 	gorm.Model
 	StudentID string `valid:"required~StudentID is required, matches(^[MDB]\\d{7}$)"`
+	FirstName string `valid:"required~FirstName is required"`
 }
